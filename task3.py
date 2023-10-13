782bbbe005a8e00618fdd22fa77314c1e5f091ab
omytemaa <120541588+omytemaa@users.noreply.github.com>
2023-10-13 23:52:09 +0300

    Delete task3.py

--- task3.py
+++
@@ -1,7 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-count = len(list_players)
-one_team = list_players[0:3]
-two_team = list_players[3:]
-print(one_team)
-print(two_team)
-
